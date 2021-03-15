46227cbae963207ee1763322d8bd722dc624cdea
Marcin M <mmaczka@inpost.pl>
2021-03-15 22:58:46 +0100

    wykorzystanie enuma na przykładzie Status

--- Kurs/Program.cs
+++ Kurs/Program.cs
@@ -6,7 +6,24 @@ namespace Kurs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kurs Szarpania - by Marek Zając");
+            Console.WriteLine("###################################");
+            Console.WriteLine("# Kurs Szarpania - by Marek Zając #");
+            Console.WriteLine("#        Developer - Marcin M.    #");
+            Console.WriteLine("#                                 #");
+            Console.WriteLine("#           moduł 5 - ex.3        #");
+            Console.WriteLine("#        wykorzystanie enuma      #");
+            Console.WriteLine("###################################\n");
+
+            string status = Status.InProgress.ToString();
+            Console.WriteLine($"Obecny status to {status}");
+            int statusInt = (int) Status.InProgress;
+            Console.WriteLine($"Obecny status to {statusInt}");
+            Status status1 = Status.InProgress;
+            statusInt = (int)status1;
+            Console.WriteLine($"Obecny status to {statusInt}");
+
+
+
         }
     }
 }
---
+++ Kurs/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs
+{
+    public enum Status
+    {
+        New = 1,
+        InProgress = 2,
+        Done = 4,
+        Closed = 8
+    }
+}
