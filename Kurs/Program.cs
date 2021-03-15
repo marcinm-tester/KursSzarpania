555f05c6d0f95ca12677165446a377c2fdfa0740
Marcin M <mmaczka@inpost.pl>
2021-03-15 22:39:37 +0100

    przeniesienie danych do klasy trapezoid

--- Kurs/Program.cs
+++ Kurs/Program.cs
@@ -6,17 +6,23 @@ namespace Kurs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kurs Szarpania - by Marek Zając");
-
+            Console.WriteLine("###################################");
+            Console.WriteLine("# Kurs Szarpania - by Marek Zając #");
+            Console.WriteLine("#        Developer - Marcin M.    #");
+            Console.WriteLine("#                                 #" );
+            Console.WriteLine("#           moduł 5 - ex.2        #");
+            Console.WriteLine("###################################\n");
             //obliczenie pola trapezu
             double a, b, h;
 
-            a = 10;
-            b = 11;
-            h = 7;
-            double trapezoidField = (a + b) * h / 2;
+            Trapezoid trapezoid = new Trapezoid();
+            trapezoid.a = 8;
+            trapezoid.b = 11;
+            trapezoid.h = 7;
 
-            Console.WriteLine($"Pole trapezu o bokach {a} i {b} oraz wysokości {h}");
+            double trapezoidField = (trapezoid.a + trapezoid.b) * trapezoid.h / 2;
+
+            Console.WriteLine($"Pole trapezu o bokach {trapezoid.a} i {trapezoid.b} oraz wysokości {trapezoid.h}");
             Console.WriteLine($"wynosi {trapezoidField}");
         }
     }
