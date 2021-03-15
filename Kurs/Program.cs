82dfaa06a6a24e89e85113bd642eb3da5d5f801e
Marcin M <mmaczka@inpost.pl>
2021-03-15 22:27:39 +0100

    obliczenie pola trapezu w metodzie Main

--- Kurs/Program.cs
+++ Kurs/Program.cs
@@ -7,6 +7,17 @@ namespace Kurs
         static void Main(string[] args)
         {
             Console.WriteLine("Kurs Szarpania - by Marek Zając");
+
+            //obliczenie pola trapezu
+            double a, b, h;
+
+            a = 10;
+            b = 11;
+            h = 7;
+            double trapezoidField = (a + b) * h / 2;
+
+            Console.WriteLine($"Pole trapezu o bokach {a} i {b} oraz wysokości {h}");
+            Console.WriteLine($"wynosi {trapezoidField}");
         }
     }
 }
