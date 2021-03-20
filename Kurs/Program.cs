7a55799fdc49e457792743fae58371bdf700e41a
Marcin M <mmaczka@inpost.pl>
2021-03-21 00:18:13 +0100

    init

--- Kurs/Program.cs
+++ Kurs/Program.cs
@@ -6,7 +6,22 @@ namespace Kurs
     {
         static void Main(string[] args)
         {
+            string Name;
+            int Number;
+
             Console.WriteLine("Kurs Szarpania - by Marek Zając");
+
+            Console.Write("Podaj imie: ");
+            Name = Console.ReadLine();
+            Console.Write("Podaj liczbe: ");
+            Number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Drogi użytkowniku {Name}.");
+            Console.WriteLine($"Uważasz, że liczba {Number} jest właściwa ?");
+            Console.WriteLine($"Czy {Number} ma dla Ciebie takie znaczenie ?");
+            Console.WriteLine($"Dziwnie {Name}, że wybrana przez Ciebie liczba to {Number}.");
+            Console.WriteLine($"A może podając {Number} miałeś na myśli {++Number} ?");
+            Console.WriteLine($"Jak sądzisz {Name} ? ");
         }
     }
 }
