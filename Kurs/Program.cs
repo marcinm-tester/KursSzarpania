cd0838f58871eec48ccfee29facabc2f1ab98970
Marcin M <mmaczka@inpost.pl>
2021-03-20 18:57:39 +0100

    add 3 classes

--- Kurs/Program.cs
+++ Kurs/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using fns = FirstNameSpace;
+using SecondNameSpace;
+using System;
 
 namespace Kurs
 {
@@ -7,6 +9,36 @@ namespace Kurs
         static void Main(string[] args)
         {
             Console.WriteLine("Kurs Szarpania - by Marek Zając");
+
+            fns.First ff1 = new fns.First();
+            fns.First ff2 = new fns.First();
+            fns.Second so1 = new fns.Second();
+            fns.Second so2 = new fns.Second();
+
+            First fs1 = new First();
+            First fs2 = new First();
+
+            ff1.Hello();
+            ff2.Hello();
+            so1.Hello();
+            so2.Hello();
+            fs1.Hello();
+            fs2.Hello();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
---
+++ Kurs/Second.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstNameSpace
+{
+    class Second
+    {
+        public void Hello()
+        {
+            Console.WriteLine($"I say 'Hello' from {this.ToString()}");
+        }
+    }
+}
