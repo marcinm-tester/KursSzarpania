8c2aea9d86872f466057cd6655182ddedb68a340
Marcin M <mmaczka@inpost.pl>
2021-03-20 19:34:03 +0100

    create two simple classes

--- Kurs/Program.cs
+++ Kurs/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace Kurs
+namespace CityBase
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kurs Szarpania - by Marek Zając");
+            Console.WriteLine("###################################");
+            Console.WriteLine("# Kurs Szarpania - by Marek Zając #");
+            Console.WriteLine("#        Developer - Marcin M.    #");
+            Console.WriteLine("#                                 #");
+            Console.WriteLine("#         Projekt: CityBase       #");
+            Console.WriteLine("#            module 6             #");
+            Console.WriteLine("###################################\n");
         }
     }
 }
