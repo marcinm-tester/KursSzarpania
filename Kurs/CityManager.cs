ba27c36d3a84f2b04a2aeb17ce93259dc3aed36c
Marcin M <mmaczka@inpost.pl>
2021-03-26 23:49:07 +0100

    add estates list

--- Kurs/CityManager.cs
+++ Kurs/CityManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CityBase.Estates;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace CityBase
 {
     class CityManager
     {
+        List<Estate> listEsatates = new List<Estate>();
     }
 }
