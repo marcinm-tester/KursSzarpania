f2a2c9a12526b3f694f39a71bc17eed45af101eb
Marcin M <mmaczka@inpost.pl>
2021-03-26 23:45:09 +0100

    solution loops

--- Kurs/Program.cs
+++ Kurs/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace Kurs
 {
@@ -10,9 +12,36 @@ namespace Kurs
             Console.WriteLine("# Kurs Szarpania - by Marek Zając #");
             Console.WriteLine("#        Developer - Marcin M.    #");
             Console.WriteLine("#                                 #");
-            Console.WriteLine("#                                 #");
-            Console.WriteLine("#                                 #");
+            Console.WriteLine("#           module 9               #");
+            Console.WriteLine("#            pętle                 #");
             Console.WriteLine("###################################\n");
+
+
+
+            List<int> lista = new List<int>();
+
+            int j;
+            string s;
+            int liczba;
+            Console.WriteLine("Podaj liczbę liczb:");
+            Console.Write("> ");
+            j = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= j ; i++)
+            {
+                Console.WriteLine($"Podaj {i}. liczbę: ");
+                liczba = int.Parse(Console.ReadLine());
+                lista.Add(liczba);
+            }
+
+            foreach (int element in lista)
+            {
+                if (element > 100)
+                    Console.WriteLine($"{element}");
+            }
+            }
         }
-    }
+
+
 }
+
