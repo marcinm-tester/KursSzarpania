ac3e6ca17da31bf1877141137b9646e292e2ef76
Marcin M <mmaczka@inpost.pl>
2021-05-02 11:37:42 +0200

    init Generyczność klas

---
+++ Kurs/AnotherGenericClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs
+{
+    interface MyInterface
+    {
+    }
+
+    class AnotherHenericClass<T>
+        where T : MyInterface
+    {
+        public T Field { get; set; }
+    }
+}
--- Kurs/Program.cs
+++ Kurs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Kurs
 {
@@ -10,9 +11,36 @@ namespace Kurs
             Console.WriteLine("# Kurs Szarpania - by Marek Zając #");
             Console.WriteLine("#        Developer - Marcin M.    #");
             Console.WriteLine("#                                 #");
-            Console.WriteLine("#                                 #");
-            Console.WriteLine("#                                 #");
+            Console.WriteLine("#           module 17             #");
+            Console.WriteLine("#       GENERYCZNOSC KLAS         #");
             Console.WriteLine("###################################\n");
+
+            var intObj = new GenericClass<int>();
+
+
+            intObj.Field = 123;
+            Console.WriteLine(intObj.Field);
+
+            var stringObj = new GenericClass<string>();
+            stringObj.Field = "Kot zjadł Alę";
+            Console.WriteLine(stringObj.Field);
+
+            var innaKlasa = new GenericClass<string>();
+            innaKlasa.Field = "1233";
+
+            Console.WriteLine(innaKlasa.Field);
+
+            var floatClass = new GenericClass<decimal>();
+            floatClass.Field = 12.345M;
+            
+            Console.WriteLine(floatClass.Field);
+            Console.WriteLine(floatClass.Field.ToString(CultureInfo.InvariantCulture.NumberFormat));
+
+
+            //nowa.Run
+
+
+
         }
     }
 }
